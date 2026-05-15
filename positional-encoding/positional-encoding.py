3d8d24a01c8d30f8ea672510efedef76648fe38a
Elijah Kurien <80718858+elijah0528@users.noreply.github.com>
2026-05-14 21:45:30 -0400

    Update solution for positional-encoding

--- positional-encoding/positional-encoding.py
+++ positional-encoding/positional-encoding.py
@@ -7,16 +7,13 @@ def positional_encoding(seq_len, d_model, base=10000.0):
     """
     # Write code here
     seq = np.arange(seq_len)[:, None]
-    dims = np.arange(d_model)[None, :]
     # Denom is a numpy array
-    denom = base ** ((2 * (dims// 2)) / d_model)
+    denom = base ** ((2 * (np.arange(d_model)// 2)) / d_model)
     angles = seq / denom
-    pe_1 = np.sin(angles)
-    pe_2 = np.cos(angles)
 
-    mask_1 = (dims % 2 == 0)
-    mask_2 = (dims % 2 == 1)
+    pe = np.empty((seq_len, d_model))
+    pe[:, 0::2] = np.sin(angles[:, 0::2])
+    pe[:, 1::2] = np.cos(angles[:, 1::2])
 
-    res = pe_1 * mask_1 + pe_2 * mask_2
-    return res
+    return pe
     
